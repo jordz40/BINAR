164e15ee2000efe0b8ebae984c86f8f8f168f165
Jordz40 <satriajorda92@gmail.com>
2022-07-08 21:26:18 +0700

    Delete bootcamp directory

--- bootcamp/OOP/main.ts
+++
@@ -1,9 +0,0 @@
-const someFunc = (someParam) => {
-    console.log(someParam)
-}
-
-someFunc("bakso")
-const sendMessageByWa = (phone, text, image) => {
-    console.log(image)
-}
-sendMessageByWa("0838", "hoy makan malem yuk","gambar")
